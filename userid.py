52fe055691bee9dc114ae2d84268e1b3a3a0905d
Stefano Antonel <stefano.antonel@epfl.ch>
2020-01-28 15:51:57 +0100

    5

--- userid.py
+++ userid.py
@@ -5,3 +5,5 @@ display.display(display.Javascript('''
    var code = "import json; sys.modules['userid'].metadata = json.loads(\'\'\'" + metadata + "\'\'\')";
    IPython.notebook.kernel.execute(code);
 '''))
+
+#ll
