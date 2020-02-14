79f1e644e2c19693868cf2b05884ebc2e4fe8613
antonel-stefano <stefano.antonel@epfl.ch>
2020-02-14 14:02:19 +0100

    Add mooc-pkg

--- single_cell_mooc_client/setup.py
+++ single_cell_mooc_client/setup.py
@@ -6,11 +6,11 @@ from setuptools import setup
 VERSION = '0.0.5'
 
 setup(
-    name='single-cell-mooc-client',
+    name='single_cell_mooc_client',
     version=VERSION,
     author='NSE',
     author_email='',
     description='Python extension to submit MOOC answers',
-    packages=['single-cell-mooc-client'],
+    packages=['single_cell_mooc_client'],
     zip_safe=False,
 )
